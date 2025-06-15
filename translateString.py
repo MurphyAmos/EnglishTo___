003b4528bf8941becc51f012a6b6059443fcade8
Murphy Amos <46387288+MurphyAmos@users.noreply.github.com>
2025-06-15 11:22:27 -0500

    Update translateString.py

--- translateString.py
+++ translateString.py
@@ -1,9 +1,26 @@
+######################################################
+#		    	 KEY 			                     #
+#	                                                 #
+#	#### = Start or End of Function 	             #
+#	                                                 #
+#	# = notes and simple comments  	                 #
+# 						                             #
+#	### = Explanation of function processes          #
+#                                                    #
+#	######### = End of Functions in Class	         #
+#						                             #
+#	##### = section identifier 		                 #
+#                                                    #
+#                                                 -MA#
+######################################################
 import speech_recognition as sr
 from deep_translator import GoogleTranslator
 
 class multiWordTranslate:
     def listen():
+    ####Start of listen()####
         def findLanguage():
+        ####Start of findLanguage####
             #making global variables to access language variables 
             global lanDictIndex    
             global Language
@@ -14,13 +31,14 @@ class multiWordTranslate:
             langDict = {"english":"en", "hindi":"hi", "spanish":"es", "dutch":"de", "french": "fr", "russian":"ru", "polish":"pl", "chinese":"zh"}
             #for loop to look through the langDict for language
             for x in langDict:
-                #while we look through if index match language landictIndex  = langDict[of full language name to return dict value]
+                ###while we look through if index match language landictIndex  = langDict[of full language name to return dict value]
                 if x == Language:
                     lanDictIndex = langDict[Language]               
                     return lanDictIndex
+        ####End of findLanguage#### 
         #making a string of what was said 
         global stringOfVoice
-        #creating voice holder on the mic, taking that an putting it into a audioholder... then turning audio into text and holding it into the stringOfVoice 
+        ###creating voice holder on the mic, taking that an putting it into a audioholder... then turning audio into text and holding it into the stringOfVoice 
         UserVoiceHolder= sr.Recognizer()
         with sr.Microphone() as source:
             audioHolder = UserVoiceHolder.listen(source)
@@ -38,14 +56,16 @@ class multiWordTranslate:
                     return stringOfVoice
                 else:
                     print("No input Was Found")
-    
+    ####End of listen()
     def returnTranslateString(string):
+    ####Start of returnTranlateString()####
         #translated string holder
         global translatedString       
-        #use googlesTramlater to translate my Userpicked language and return the string
+        ###use googlesTramlater to translate my Userpicked language and return the string
         translatedString = GoogleTranslator(source='auto',target = lanDictIndex).translate(string)
         print(f"Translated to {Language.title()}: {translatedString}")
         return translatedString
-
+    ####end of  returnTranlateString()####
+    ######### Run Sequence #########
     #run Return and use the output of multiwordString as the input of it
     returnTranslateString(listen())
